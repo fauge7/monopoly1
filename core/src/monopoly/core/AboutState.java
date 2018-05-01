aa96306943de63a14c3fab431aff7373ba500172
ElementUnknown <38635378+ElementUnknown@users.noreply.github.com>
2018-04-30 17:02:44 -0700

    Add files via upload

--- core/src/monopoly/core/AboutState.java
+++ core/src/monopoly/core/AboutState.java
@@ -1,8 +1,13 @@
 package monopoly.core;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
 public class AboutState extends State{
 
@@ -23,6 +28,11 @@ public class AboutState extends State{
 	private float timer;
 
 	private SpriteBatch batch;
+	
+	private BitmapFont fontTitle;
+  private BitmapFont fontDescription;
+  private BitmapFont fontRoll;
+  private BitmapFont fontHouse;
 
 	public AboutState(StateManager manager, SpriteBatch batch){
 		super(manager);
@@ -32,6 +42,29 @@ public class AboutState extends State{
 		aboutPage = new Texture(Gdx.files.internal("assets/about_page.png"));
 		backUp = new Texture(Gdx.files.internal("assets/back_up.png"));
 		backDown = new Texture(Gdx.files.internal("assets/back_down.png"));
+
+		FreeTypeFontGenerator fontGeneratorBH = new FreeTypeFontGenerator(Gdx.files.internal("assets/blue_highway_d.ttf"));
+    FreeTypeFontGenerator fontGeneratorBH2 = new FreeTypeFontGenerator(Gdx.files.internal("assets/blue_highway_rg.ttf"));
+    FreeTypeFontGenerator fontGeneratorBM = new FreeTypeFontGenerator(Gdx.files.internal("assets/pixel_emulator.otf"));
+    
+  //Creates the fonts needed
+    FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+    parameter.size = 28;
+    fontTitle = fontGeneratorBH.generateFont(parameter);
+    parameter.size = 24;
+    fontDescription = fontGeneratorBH2.generateFont(parameter);
+    parameter.size = 42;
+    fontRoll = fontGeneratorBH.generateFont(parameter);
+    parameter.size = 20;
+    fontHouse = fontGeneratorBH.generateFont(parameter);
+
+    //Sets font colors to black
+    fontHouse.setColor(new Color(0,0,0,1));
+    fontTitle.setColor(new Color(0,0,0,1));
+    fontDescription.setColor(new Color(0,0,0,1));
+    fontRoll.setColor(new Color(0,0,0,1));
+    
+    
 
 		backX = 50;
 		backY = 50;
@@ -93,7 +126,9 @@ public class AboutState extends State{
 		}else{
 			batch.draw(backUp, backX, backY);
 		}
-
+		GlyphLayout lay = new GlyphLayout(fontDescription, "Win Monoploy by not going in debt and owning money!");
+		fontDescription.draw(batch, "Win Monoploy by not going in debt and owning money!", Gdx.graphics.getWidth() / 2 - lay.width / 2, Gdx.graphics.getHeight() /2);
+		
 		batch.end();
 
 	}
